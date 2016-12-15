90b1d9e0a94b8fdd0d303fd75e9d819edc8f7495
hazim1093 <hazim_malik@hotmail.com>
2016-12-15 15:33:20 +0500

    Fix spelling mistake

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-_elaticsearch_version=$1
-_elaticsearch_tag=$2
+_elasticsearch_version=$1
+_elasticsearch_tag=$2
 _release_build=false
 
-if [ -z "${_elaticsearch_version}" ]; then
+if [ -z "${_elasticsearch_version}" ]; then
 	source ELASTICSEARCH_VERSION
-	_elaticsearch_version=$ELASTICSEARCH_VERSION
-	_elaticsearch_tag=$ELASTICSEARCH_VERSION
+	_elasticsearch_version=$ELASTICSEARCH_VERSION
+	_elasticsearch_tag=$ELASTICSEARCH_VERSION
 	_release_build=true
 fi
 
-echo "ELASTICSEARCH_VERSION: ${_elaticsearch_version}"
-echo "DOCKER TAG: ${_elaticsearch_tag}"
+echo "ELASTICSEARCH_VERSION: ${_elasticsearch_version}"
+echo "DOCKER TAG: ${_elasticsearch_tag}"
 echo "RELEASE BUILD: ${_release_build}"
 
-docker build --build-arg ELASTICSEARCH_VERSION=${_elaticsearch_version} --tag "stakater/elaticsearch:${_elaticsearch_tag}"  --no-cache=true .
+docker build --build-arg ELASTICSEARCH_VERSION=${_elasticsearch_version} --tag "stakater/elasticsearch:${_elasticsearch_tag}"  --no-cache=true .
 
 if [ $_release_build == true ]; then
-	docker build --build-arg ELASTICSEARCH_VERSION=${_elaticsearch_version} --tag "stakater/elaticsearch:latest"  --no-cache=true .
+	docker build --build-arg ELASTICSEARCH_VERSION=${_elasticsearch_version} --tag "stakater/elasticsearch:latest"  --no-cache=true .
 fi
