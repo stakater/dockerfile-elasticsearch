c87bcb33f29c8ce3abbc119b84fe3e064e628166
hazim1093 <hazim_malik@hotmail.com>
2017-02-21 15:43:22 +0500

    Add resources for elasticsearch v5.2.1

---
+++ 5.2/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+_elasticsearch_version=$1
+_elasticsearch_tag=$2
+_release_build=false
+
+if [ -z "${_elasticsearch_version}" ]; then
+	source ELASTICSEARCH_VERSION
+	_elasticsearch_version=$ELASTICSEARCH_VERSION
+	_elasticsearch_tag=$ELASTICSEARCH_VERSION
+	_release_build=true
+fi
+
+echo "ELASTICSEARCH_VERSION: ${_elasticsearch_version}"
+echo "DOCKER TAG: ${_elasticsearch_tag}"
+echo "RELEASE BUILD: ${_release_build}"
+
+docker build --build-arg ELASTICSEARCH_VERSION=${_elasticsearch_version} --tag "stakater/elasticsearch:${_elasticsearch_tag}"  --no-cache=true .
+
+if [ $_release_build == true ]; then
+	docker tag "stakater/elasticsearch:${_elasticsearch_tag}" "stakater/elasticsearch:latest"
+fi
---
+++ 5.2/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${COMMAND:0:1}" = '-' ]; then
+	set -- elasticsearch "$COMMAND"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$COMMAND" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R stakater:stakater "$path"
+	done
+
+	set -- su-exec stakater "$COMMAND"
+	su-exec stakater "$COMMAND"
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$COMMAND"
