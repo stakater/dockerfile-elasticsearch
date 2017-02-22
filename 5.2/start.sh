faa5d3b407f1004f0cc14448408d5ad33b7abf2c
hazim1093 <hazim_malik@hotmail.com>
2017-02-22 17:23:21 +0500

    Update start script

--- 5.2/start.sh
+++ 5.2/start.sh
@@ -8,7 +8,8 @@ fi
 
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
-if [ "$COMMAND" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+if [ "$COMMAND" = 'elasticsearch' -a "$(id -u)" = '0' ];
+then
 	# Change the ownership of user-mutable directories to elasticsearch
 	for path in \
 		/usr/share/elasticsearch/data \
@@ -19,9 +20,9 @@ if [ "$COMMAND" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 
 	set -- su-exec stakater "$COMMAND"
 	su-exec stakater "$COMMAND"
+else
+	# As argument is not related to elasticsearch,
+	# then assume that user wants to run his own process,
+	# for example a `bash` shell to explore this image
+	exec "$COMMAND"
 fi
-
-# As argument is not related to elasticsearch,
-# then assume that user wants to run his own process,
-# for example a `bash` shell to explore this image
-exec "$COMMAND"
